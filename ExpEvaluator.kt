ced1a95b20309ced9572e7c722743078caf6fdfc
SteamsDev <dev208@msn.com>
2020-12-03 14:36:59 -0500

    Update ExpEvaluator.kt

--- ExpEvaluator.kt
+++ ExpEvaluator.kt
@@ -4,13 +4,13 @@ open class ExpEvaluator {
         if (getLhs() != tree) {
             var treeLeft = tree.getLhs()
             var treeRight = tree.getRhs()
-            when tree.value ()({
+            when (tree.value()){
                 "PLUS" -> return evaluate(treeLeft)+evaluate(treeRight)
                 "MINUS" -> return evaluate(treeLeft)-evaluate(treeRight)
                 "TIMES" -> return evaluate(treeLeft)*evalate(treeRight)
                 "DIVIDES" -> return evaluate(treePeft)/evaluate(treeRight)
                 "POWER" -> return evaluate(treeLeft).pow(evaluate(treeRight))
-            })
+            }
         }
         else {
             return tree.value
