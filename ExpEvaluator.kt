a7344e739f58b0292bb939b60197344e65704bbd
SteamsDev <dev208@msn.com>
2020-12-03 14:12:27 -0500

    Add files via upload

---
+++ ExpEvaluator.kt
@@ -0,0 +1,20 @@
+open class ExpEvaluator {
+
+    fun evaluate (tree : treeNode) {
+        if (getLhs() != tree) {
+            var treeLeft = tree.getLhs()
+            var treeRight = tree.getRhs()
+            when tree.value ()({
+                "PLUS" -> return evaluate(treeLeft)+evaluate(treeRight)
+                "MINUS" -> return evaluate(treeLeft)-evaluate(treeRight)
+                "TIMES" -> return evaluate(treeLeft)*evalate(treeRight)
+                "DIVIDES" -> return evaluate(treePeft)/evaluate(treeRight)
+                "POWER" -> return evaluate(treeLeft).pow(evaluate(treeRight))
+            })
+        }
+        else {
+            return tree.value
+        }
+    }
+
+}
